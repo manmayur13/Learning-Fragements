0492855ff8e6b000b196b34ca9b2d3259214124d
manmayur13 <patilmayur16777@gmail.com>
2024-07-03 12:04:21 +0530

    Intital Commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <> 
+      <h1>Healthy Foods</h1>
+      <ul class="list-group">
+        <li class="list-group-item" aria-disabled="true">
+          Daal
+        </li>
+        <li class="list-group-item">Salad</li>
+        <li class="list-group-item">Green sabji</li>
+        <li class="list-group-item">Khichadi</li>
+        <li class="list-group-item">Fruits</li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
